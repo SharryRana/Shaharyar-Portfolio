4eee5feffca86f5b0a409efc0ed064a92c12b3b2
shaharyar-rana <hshaharyar@zolvat.com>
2026-05-30 11:40:58 +0500

    chore: update database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +16,17 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'username' => 'testuser',
-            'phone' => '123-456-7890',
-        ]);
+        User::firstOrCreate(
+            ['email' => 'test@example.com'],
+            [
+                'name' => 'Test User',
+                'username' => 'testuser',
+                'phone' => '123-456-7890',
+                'password' => Hash::make('password'),
+            ]
+        );
+
+        $this->call(TeamMemberSeeder::class);
+        $this->call(PortfolioContentSeeder::class);
     }
 }
