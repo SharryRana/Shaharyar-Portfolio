64587051565e0f2c63e0524dcbd7bb900a98d640
shaharyar-rana <hshaharyar@zolvat.com>
2026-07-09 15:24:00 +0500

    feat(blog-admin): add shared confirmation modal

--- Modules/Blog/resources/views/admin/layouts/master.blade.php
+++ Modules/Blog/resources/views/admin/layouts/master.blade.php
@@ -40,6 +40,29 @@
         </div>
     </div>
 
+    <div class="modal fade admin-confirm-modal" id="adminConfirmModal" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <div>
+                        <div class="admin-confirm-icon">
+                            <i class="bi bi-exclamation-triangle"></i>
+                        </div>
+                        <h5 class="modal-title" id="adminConfirmTitle">Confirm Action</h5>
+                    </div>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body" id="adminConfirmMessage">
+                    Are you sure you want to continue?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Cancel</button>
+                    <button type="button" class="btn btn-danger" id="adminConfirmAction">Delete</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <!-- Bootstrap & jQuery -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
